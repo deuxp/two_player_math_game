b561e10ddcac4ed0c721969b9461eb8e6ce3466e
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-07 00:01:54 -0400

    flesh out the plan

--- index.rb
+++ index.rb
@@ -12,42 +12,36 @@
 #             - [ ] if life is zero game over
 #         - [ ] then next players turn
 
-class Player
-    attr_accessor :name, :lives, :dead?, :score
-    
-    def initialize(name)
-        @name = name
-        @lives = 3
-        @score = 0
-        @dead = false
-    end
-end
 
 # game
 class GameEngine
     # while loop game environment
-    def start 
+
+    def start
+        # ask for player names
+        # init the 2 characters -> they live in the game
+        
         # While player1 lives > 0 AND player2 lives > 0 # ---------------------------
-
+        
         # get the 2 numbers from the prompt
-
-
+        
+        
         # ask player1
         #     get player1 guess - await
         #         store it
-
+        
         # ask player2
         #     get player2 guess - await
         #         store it
-
-
-
+        
+        
+        
         #  player1 = gets.chomp
-
+        
         # #  prompt > choose 2 numbers
         # #  > 5
         #  > 3
-
+        
     end # ----------------------------------------------------------------------
     
     # end game
@@ -56,27 +50,37 @@ class GameEngine
     end
 end
 
-class Match < GameEngine
-    attr_accessor :p1, :p2, :p1_guess, :p2_guess
+class Player
+    attr_accessor :name, :lives, :dead, :score
     
-    # pass in the player instance: so @p1.name -> 'name' etc
-    def initialize(p1_name, p2_name)
-        @p1 = Player.new(p1_name)
-        @p2 = Player.new(p2_name)
-        @p1_guess = nil
-        @p2_guess = nil
+    def initialize(name)
+        @name = name
+        @lives = 3
+        @score = 0
     end
-
-
-
 end
 
-# Get the names from prompt
+class turn
+    attr_accessor :p1_answer, :p2_answer
+    
+    def initialize
+        @p1_answer = nil
+        @p2_answer = nil
+    end
+    
+end
 
-# what is your name?
+# start screen:
 
-# a = gets.chomp
-# b = gets.chomp
+puts "Player 1. What is your name?"
+p1_name = gets.chomp
+puts "Player 2. What is your name?"
+p2_name = gets.chomp
 
-match1 = Match.new(a, b)
-match1.start
+
+@p1 = Player.new(p1_name)
+@p2 = Player.new(p2_name)
+
+puts "p1: #{@p1.name} || p2: #{@p2.name}"
+# match1 = Match.new(a, b)
+# match1.start
