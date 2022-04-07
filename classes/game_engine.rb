13b3db038b1e773a868730dc2ace9e15b524e6c0
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-07 15:01:32 -0400

    rough out the logic for the game

--- classes/game_engine.rb
+++ classes/game_engine.rb
@@ -1,34 +1,44 @@
-require_relative 'classes/player'
-require_relative 'classes/question'
-
-# game
 class GameEngine
-    # while loop game environment
 
     def start
-        # ask for player names
-        puts "Player 1. What is your name?"
-        name1 = gets.chomp
-        puts "Player 2. What is your name?"
-        name2 = gets.chomp
+        # # ask for player names
+        # puts "Player 1. What is your name?"
+        # name1 = gets.chomp
+        # puts "Player 2. What is your name?"
+        # name2 = gets.chomp
 
-        p1 = Player.new(name1)
-        p2 = Player.new(name2)
+        # p1 = Player.new(name1)
+        # p2 = Player.new(name2)
+
+        p1 = Player.new('Elaine')
+        p2 = Player.new('Jerry')
+        counter = 0
         
-        # While player1 lives > 0 AND player2 lives > 0 # ---------------------------
-        # start screen:
+        # --------------------------- --------------------------- 
+        while p1.lives > 0 and p2.lives > 0
+        # whos turn?
+        turn = counter % 2
+        players = [p1, p2]
+        puts "--- --- --- #{players[turn].name}'s turn --- --- ---"
+        # p players[turn]
         
+        # ask player
+        question = Question.new
+        puts "What is #{turn.num1} + #{turn.num2} ?"
+        guess = gets.chomp.to_i # this is a string -> convert to num
         
-        # ask player1
-        #     get player1 guess - await
-        #         store it
+        if guess == question.answer
+            puts "Yay! You answered correctly"
+        end
         
-        # ask player2
-        #     get player2 guess - await
-        #         store it
-        
-        puts "p1 name: #{p1.name} and p2 name: #{p2.name}"
-    end # ----------------------------------------------------------------------
-    
-   
+        if guess != question.answer
+            players[turn].reduce_life
+            puts "ouch! wrong answer"
+        end
+        counter += 1
+        end
+        # declare winner
+        p p1
+        p p2
+    end # --------------------------- --------------------------- 
 end
