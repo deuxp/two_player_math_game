be9cdf6624c852052bed9887dc04704b6cd8e659
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-07 16:22:34 -0400

    create title screen module

---
+++ modules/title_screen.rb
@@ -0,0 +1,10 @@
+module TitleScreen
+    def banner
+        puts "
+        ╔╦╗┌─┐┌┬┐┬ ┬  ╔═╗┌─┐┌┬┐┌─┐
+        ║║║├─┤ │ ├─┤  ║ ╦├─┤│││├┤ 
+        ╩ ╩┴ ┴ ┴ ┴ ┴  ╚═╝┴ ┴┴ ┴└─┘
+         - Press ENTER to start -"
+        gets.chomp
+    end
+end
