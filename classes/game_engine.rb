edaedfc35af355c6b53b781097daeb828aa17117
Gottfried Kleinberger <friedi.frieds@gmail.com>
2022-04-07 15:52:18 -0400

    happy path for game engine complete; TODO: guard against incorrect entry type

--- classes/game_engine.rb
+++ classes/game_engine.rb
@@ -1,34 +1,36 @@
 class GameEngine
 
     def start
-        # # ask for player names
-        # puts "Player 1. What is your name?"
-        # name1 = gets.chomp
-        # puts "Player 2. What is your name?"
-        # name2 = gets.chomp
+        # ask for player names
+        puts "Player 1. What is your name?"
+        name1 = gets.chomp
+        puts "Player 2. What is your name?"
+        name2 = gets.chomp
 
-        # p1 = Player.new(name1)
-        # p2 = Player.new(name2)
-
-        p1 = Player.new('Elaine')
-        p2 = Player.new('Jerry')
+        p1 = Player.new(name1)
+        p2 = Player.new(name2)
         counter = 0
         
+        # Game Engine
         # --------------------------- --------------------------- 
         while p1.lives > 0 and p2.lives > 0
         # whos turn?
         turn = counter % 2
         players = [p1, p2]
-        puts "--- --- --- #{players[turn].name}'s turn --- --- ---"
-        # p players[turn]
+
+        puts "
+        
+        --- --- --- #{players[turn].name}'s turn --- --- ---
+        "
         
         # ask player
         question = Question.new
-        puts "What is #{turn.num1} + #{turn.num2} ?"
-        guess = gets.chomp.to_i # this is a string -> convert to num
+        puts "What is #{question.num1} + #{question.num2} ?"
+        # TODO: add guard for integer entry
+        guess = gets.chomp.to_i
         
         if guess == question.answer
-            puts "Yay! You answered correctly"
+            puts "Correct!"
         end
         
         if guess != question.answer
@@ -38,7 +40,11 @@ class GameEngine
         counter += 1
         end
         # declare winner
-        p p1
-        p p2
+
+        puts "
+        # ###  #{p1.lives > 0 ? p1.name : p2.name} is the winner  ### #
+        "
+        puts "Score: #{p1.name} #{p1.lives}/3 --- #{p2.name} #{p2.lives}/3
+        "
     end # --------------------------- --------------------------- 
 end
